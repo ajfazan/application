2f99e911d1a557fd8a56136fa3b48d32ed981e2b
Antonio Juliano Fazan <ajfazan@localhost.localdomain>
2015-09-28 17:24:47 -0300

    Update package application

--- dem_diff/main.cpp
+++ dem_diff/main.cpp
@@ -8,20 +8,20 @@
 
 #include <string>
 
-typedef std::vector<string> container_type;
+typedef std::vector<std::string> container_type;
 
 typedef std::pair<std::string,std::string> dem_pair;
 
 bool is_equal( const dem_pair& p1, const dem_pair& p2 )
 {
   std::string f1[] = {
-    boost::filesystem::path( p1.first  ).basename(),
-    boost::filesystem::path( p1.second ).basename()
+    boost::filesystem::path( p1.first  ).filename().string(),
+    boost::filesystem::path( p1.second ).filename().string()
   };
 
   std::string f2[] = {
-    boost::filesystem::path( p2.first  ).basename(),
-    boost::filesystem::path( p2.second ).basename()
+    boost::filesystem::path( p2.first  ).filename().string(),
+    boost::filesystem::path( p2.second ).filename().string()
   };
 
   return ( ( ( f1[0] == f2[0] ) && ( f1[1] == f2[1] ) ) ||
@@ -45,11 +45,11 @@ int main( int argc, char* argv[] )
 
   for( it1 = dem_set->begin(); it1 != end; ++it1 ) {
 
-    image32 base( *it1 );
+    canvas::image32 base( *it1 );
 
-    for( it2 = boost::next( it1 ); it2 != end; ++it2 ) {
+    for( it2 = boost::next( it1 ); it2 != dem_set->end(); ++it2 ) {
 
-      if( base.intersects( image32( *it2 ) ) ) {
+      if( base.intersects( canvas::image32( *it2 ) ) ) {
 
         pairs.push_back( std::make_pair( *it1, *it2 ) );
       }
@@ -66,11 +66,12 @@ int main( int argc, char* argv[] )
 
   for( p_it = pairs.begin(); p_it != p_end; ++p_it ) {
 
-    image32 base( new image32( p_it->first ) );
+    canvas::image32 dem1( p_it->first  );
+    canvas::image32 dem2( p_it->second );
 
-    image32::ptr diff( base.compute_difference( image32( p_it->second ) ) );
+    canvas::image32::ptr diff( dem1.compute_difference( dem2 ) );
 
-    image32::stats s( diff->compute_stats() );
+    canvas::image32::stats s( diff->compute_stats() );
   }
 
   return 0;
